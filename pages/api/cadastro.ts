e87e3fca4e6a81c2d0b575c0896f2aaeb46ef6a0
CezarNeto37 <cezarmartins@msn.com>
2022-04-15 15:41:31 -0300

    models

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -1,18 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { RespostaPadraoMsg } from '../../types/respostapadraomsg';
 import type { CadastroRequisicao } from '../../types/CadastroRequisicao';
+import { UsuarioModel } from '../../models/usuarioModels';
 
-const endpointCadastro = (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+const endpointCadastro =
+    async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
 
-    if (req.method === 'POST') {
-        const usuario = req.body as CadastroRequisicao;
+        if (req.method === 'POST') {
+            const usuario = req.body as CadastroRequisicao;
 
-        if (!usuario.nome || usuario.nome.length < 2) {
-            return res.status(400).json({ erro: 'Nome invalido' });
+            if (!usuario.nome || usuario.nome.length < 2) {
+                return res.status(400).json({ erro: 'Nome invalido' });
+            }
+            if (!usuario.email || usuario.email.length < 5 || !usuario.email.includes('@' || !usuario.email.includes('.'))) {
+                return res.status(400).json({ erro: 'Email invalido' });
+            }
+            if (!usuario.senha || usuario.senha.length < 4) {
+                return res.status(400).json({ erro: 'Senha invalida' });
+            }
+            await UsuarioModel.create(usuario);
+            return res.status(200).json({ msg: 'Usuario criado com sucesso' });
+
         }
-        if (!usuario.email || usuario.email.length < 5 || !usuario.email.includes('@' || !usuario.email.includes('.'))) {
-
-        }
+        return res.status(405).json({ erro: 'Metodo informado nao e valido' });
     }
-    return res.status(405).json({ erro: 'Metodo informado nao e valido' });
-}
+
+export default endpointCadastro;
